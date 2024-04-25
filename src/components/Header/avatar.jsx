e314bc1465be5408c8a4dcbbde1ece129f88416a
Pedro Grunwald <pedro.grunwald.com>
2024-04-25 07:08:26 -0300

    feat: Components Header

---
+++ src/components/Header/avatar.jsx
@@ -0,0 +1,11 @@
+export default function Avatar() {
+  return (
+    <img
+      src="/user.svg"
+      alt="User"
+      className="rounded-full border-2 border-zinc-400"
+      width={48}
+      height={48}
+    />
+  );
+}
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import Logo from "./Logo";
+import SearchForm from "./SearchForm";
+import Avatar from "./avatar";
+
+export default function Header() {
+  return (
+    <header className="px-4 xl:px-6 h-16 lg:h-20 flex items-center justify-between gap-6 md:gap-16 xl:gap-32 bg-zinc-100 dark:bg-zinc-800">
+      <Logo />
+      <SearchForm />
+      <Avatar />
+    </header>
+  );
+}
