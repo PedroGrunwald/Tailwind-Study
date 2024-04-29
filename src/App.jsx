a9c4a3771a367a6578cf3e9d57aa4462247ce778
Pedro Grunwald <pedro.grunwald.com>
2024-04-29 07:42:39 -0300

    feat: Bottombar components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Header from "./components/Header";
 import Container from "./components/container";
 import Sidebar from "./components/Sidebar/sidebar";
+import Bottombar from "./components/BottomBar";
 
 export default function App() {
   const [count, setCount] = useState(0);
@@ -11,19 +12,22 @@ export default function App() {
       <Header />
       <Container>
         <div className="flex flex-col md:flex-row gap-2 xl:gap-4 h-full relative ">
-        <Sidebar/>
-        <h1 className="text-3xl text-center text-red-600">
-          Ready to start with Tailwind!
-        </h1>
-        <button
-          className="
+          <Sidebar />
+          <div>
+            <h1 className="text-3xl text-center text-red-600">
+              Ready to start with Tailwind!
+            </h1>
+            <button
+              className="
         block w-fit mx-auto my-8 px-4 py-2 rounded-lg
         border-transparent hover:border-cyan-500 border-[1px]
         transition-colors"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </button>
+              onClick={() => setCount((count) => count + 1)}
+            >
+              count is {count}
+            </button>
+          </div>
+          <Bottombar/>
         </div>
       </Container>
     </>
